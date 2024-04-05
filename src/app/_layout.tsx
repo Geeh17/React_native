c3f1e6b079e1e0a65f230ebb4df0b33e66087ef5
113297903+Geeh17@users.noreply.github.com <141880337+GldaSilva17@users.noreply.github.com>
2024-04-05 00:11:57 -0300

    Criação das imagens, rotas, configuração do projeto, tela inicial de compra

---
+++ src/app/_layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/global.css";
+
+import { Slot } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
+import { Loading } from "@/components/loading";
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+  return <Slot />;
+}
