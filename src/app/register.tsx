df7330632fdb43c2268726eca17f3b78620e7c41
113297903+Geeh17@users.noreply.github.com <141880337+GldaSilva17@users.noreply.github.com>
2024-04-06 17:32:31 -0300

    criação do ticket, credential, header e qrcode.

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,6 +1,7 @@
-import { View, Image, StatusBar } from "react-native";
+import { useState } from "react";
+import { View, Image, StatusBar, Alert } from "react-native";
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 
 import { colors } from "@/styles/colors";
 
@@ -8,16 +9,25 @@ import { Input } from "@/components/input";
 import { Button } from "@/components/button";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert("Inscrição", "Preencha todos os campos!");
+    }
+
+    router.push("/ticket");
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
-
       <Image
         source={require("@/assets/logo.png")}
         className="h-16"
         resizeMode="contain"
       />
-
       <View className="w-full mt-12 gap-3">
         <Input>
           <FontAwesome6
@@ -25,7 +35,6 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Nome completo" />
         </Input>
 
         <Input>
@@ -34,10 +43,14 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+          <Input.Field
+            placeholder="E-mail"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+          />
         </Input>
 
-        <Button title="Realizar inscrição" />
+        <Button title="Realizar inscrição" onPress={handleRegister} />
 
         <Link
           href="/"
